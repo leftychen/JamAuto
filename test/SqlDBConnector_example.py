06cdf5e4f9afb390d5360756f74d666dc38022a5
leftychen <lefty-chen@hotmail.com>
2016-06-06 00:51:58 -0400

    sqlDBconnector test changes

--- test/SqlDBConnector_example.py
+++ test/SqlDBConnector_example.py
@@ -1,4 +1,4 @@
-from temp.sqldbconnector.dbConnector import SqlDBConnector
+from temp.Leftychen.sqldbconnector.dbConnector import SqlDBConnector
 
 conn = SqlDBConnector("jamtest", "jamtest", "jamauto_stockmkt_data")
 df = conn.get_HS300_Current_Data(('2010-04-16', '2010-05-16'))
